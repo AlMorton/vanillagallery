b2bc1454a2e90fe4a3f395acdb13631b0d839459
Al Morton <morton_al@hotmail.com>
2020-01-02 13:15:09 +0900

    Latest changes

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,30 @@
 function setUp() {
+    
+    const container = document.querySelector('.image-container');
+    
+    const carouselContainer = document.querySelector('.carousel-container');  
 
-    const container = document.getElementsByClassName('image-container')[0];
+    const images = container.querySelectorAll('img');
 
-    const images = container.getElementsByTagName('img');
+    const miniImages = [];
+
+    images.forEach(image => {
+        var imageNode = document.createElement('img');
+        imageNode.src = image.src;      
+            
+        var divNode = document.createElement('div');
+        divNode.className = 'carousel-item';
+        divNode.appendChild(imageNode); 
+        
+        carouselContainer.appendChild(divNode);
+        miniImages.push(divNode);
+    });
+    
+
+    let currentImage = images[0];
+    let highlightedImage = miniImages[0];
+
+    highlightedImage.classList.add('highlighted');    
 
     for (let index = 1; index < images.length; index++) {
 
@@ -16,23 +38,34 @@ function setUp() {
 
     const leftButton = buttons[0];
 
-    const rightButton = buttons[1];
+    const rightButton = buttons[1];    
     
     let counter = 0;
 
     leftButton.addEventListener('click', () => {     
-           
+        
+        currentImage.style.display = 'none';
+        highlightedImage.classList.remove('highlighted'); 
+
         counter--;
 
         if(counter < min) {
-            counter = max;
-        }
 
-        images[counter + 1].style.display = 'none';
-        images[counter].style.display = 'flex';
+            counter = max;
+        }              
+
+        currentImage = images[counter];
+        highlightedImage = miniImages[counter];
+
+        currentImage.style.display = 'flex';
+        highlightedImage.classList.add('highlighted'); 
+
     });
 
     rightButton.addEventListener('click', () => {
+
+        currentImage.style.display = 'none';
+        highlightedImage.classList.remove('highlighted');  
 
         counter++;
 
@@ -40,9 +73,12 @@ function setUp() {
         {
             counter = min;
         }
-
-        images[counter - 1].style.display = 'none';
-        images[counter].style.display = 'flex';
+        
+        currentImage = images[counter];
+        highlightedImage = miniImages[counter];
+        
+        currentImage.style.display = 'flex'; 
+        highlightedImage.classList.add('highlighted');         
     });    
 }
 
